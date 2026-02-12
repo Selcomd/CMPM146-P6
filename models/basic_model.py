f26aaf03831c530c8e47efb83b674d8acbab8f8a
“Yahir <yarico@ucsc.edu>
2026-02-11 17:23:45 -0800

    Initial P6 submission

---
+++ models/basic_model.py
@@ -0,0 +1,39 @@
+from models.model import Model
+from tensorflow.keras import Sequential, layers
+from tensorflow.keras.layers.experimental.preprocessing import Rescaling
+from tensorflow.keras.optimizers import RMSprop
+
+
+class BasicModel(Model):
+
+    def _define_model(self, input_shape, categories_count):
+
+        self.model = Sequential([
+            Rescaling(1./255, input_shape=input_shape),
+
+            layers.Conv2D(16, (3,3), activation='relu'),
+            layers.MaxPooling2D((2,2)),
+
+            layers.Conv2D(16, (3, 3), activation='relu'),
+            layers.MaxPooling2D((2, 2)),
+
+            layers.Conv2D(32, (3,3), activation='relu'),
+            layers.MaxPooling2D((2,2)),
+
+            layers.Conv2D(64, (3,3), activation='relu'),
+            layers.MaxPooling2D((2,2)),
+
+            layers.Flatten(),
+
+            layers.Dense(10, activation='relu'),
+            layers.Dropout(0.3),
+
+            layers.Dense(categories_count, activation='softmax')
+        ])
+
+    def _compile_model(self):
+        self.model.compile(
+            optimizer=RMSprop(learning_rate=0.001),
+            loss='categorical_crossentropy',
+            metrics=['accuracy']
+        )
