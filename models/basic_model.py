4f3c7431b16fd053fa5d655ce1539e16024373f3
kirilbud <kiril.saltz@gmail.com>
2026-02-15 09:51:49 -0800

    72% still not done but making sure to push this time fr fr

--- models/basic_model.py
+++ models/basic_model.py
@@ -1,6 +1,6 @@
 from models.model import Model
 from tensorflow.keras import Sequential, layers
-from tensorflow.keras.layers.experimental.preprocessing import Rescaling
+from tensorflow.keras.layers.experimental.preprocessing import Rescaling ,RandomFlip, RandomRotation
 from tensorflow.keras.optimizers import RMSprop
 
 
@@ -10,11 +10,13 @@ class BasicModel(Model):
 
         self.model = Sequential([
             Rescaling(1./255, input_shape=input_shape),
+            #RandomFlip("horizontal"),
+            RandomRotation(.001),
 
             layers.Conv2D(16, (3,3), activation='relu'),
             layers.MaxPooling2D((2,2)),
 
-            layers.Conv2D(16, (3, 3), activation='relu'),
+            layers.Conv2D(32, (3, 3), activation='relu'),
             layers.MaxPooling2D((2, 2)),
 
             layers.Conv2D(32, (3,3), activation='relu'),
@@ -23,17 +25,22 @@ class BasicModel(Model):
             layers.Conv2D(64, (3,3), activation='relu'),
             layers.MaxPooling2D((2,2)),
 
+            layers.Conv2D(128, (3,3), activation='relu'),
+            layers.MaxPooling2D((2,2)),
+            layers.Dropout(0.1),
+
             layers.Flatten(),
 
-            layers.Dense(10, activation='relu'),
-            layers.Dropout(0.3),
+            layers.Dense(32, activation='relu'),
+            layers.Dense(64, activation='relu'),
+            layers.Dropout(0.5),
 
             layers.Dense(categories_count, activation='softmax')
         ])
 
     def _compile_model(self):
         self.model.compile(
-            optimizer=RMSprop(learning_rate=0.001),
+            optimizer=RMSprop(learning_rate=0.0008),
             loss='categorical_crossentropy',
             metrics=['accuracy']
         )
