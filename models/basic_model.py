8c9ea379ffd2162414b7ed0ab966f79aa9a94182
kirilbud <76541897+kirilbud@users.noreply.github.com>
2026-02-16 17:13:36 -0800

    73%

--- models/basic_model.py
+++ models/basic_model.py
@@ -1,6 +1,6 @@
 from models.model import Model
 from tensorflow.keras import Sequential, layers
-from tensorflow.keras.layers.experimental.preprocessing import Rescaling
+from tensorflow.keras.layers.experimental.preprocessing import Rescaling, RandomFlip, RandomRotation
 from tensorflow.keras.optimizers import RMSprop
 
 
@@ -10,22 +10,30 @@ class BasicModel(Model):
 
         self.model = Sequential([
             Rescaling(1./255, input_shape=input_shape),
+            RandomFlip("horizontal"),
+            RandomRotation(0.01),
 
             layers.Conv2D(16, (3,3), activation='relu'),
             layers.MaxPooling2D((2,2)),
 
-            layers.Conv2D(16, (3, 3), activation='relu'),
-            layers.MaxPooling2D((2, 2)),
-
             layers.Conv2D(32, (3,3), activation='relu'),
             layers.MaxPooling2D((2,2)),
+
+
+            layers.Conv2D(48, (3, 3), activation='relu'),
+            layers.MaxPooling2D((2, 2)),
 
             layers.Conv2D(64, (3,3), activation='relu'),
             layers.MaxPooling2D((2,2)),
 
+            layers.Conv2D(128, (3,3), activation='relu'),
+            layers.MaxPooling2D((2,2)),
+            layers.Dropout(0.1),
+
             layers.Flatten(),
 
-            layers.Dense(10, activation='relu'),
+            
+            layers.Dense(32, activation='relu'),
             layers.Dropout(0.3),
 
             layers.Dense(categories_count, activation='softmax')
@@ -33,7 +41,8 @@ class BasicModel(Model):
 
     def _compile_model(self):
         self.model.compile(
-            optimizer=RMSprop(learning_rate=0.001),
+            optimizer=RMSprop(learning_rate=0.0015),
             loss='categorical_crossentropy',
             metrics=['accuracy']
         )
+
